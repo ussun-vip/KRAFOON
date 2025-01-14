f7b4a42acca99b7dcc3409819c6c2dd3adfb6662
Ussun <159697872+ussun-vip@users.noreply.github.com>
2025-01-14 05:40:24 -0800

    Update krafoon.js

--- netlify/functions/krafoon.js
+++ netlify/functions/krafoon.js
@@ -1,1 +1,30 @@
-
+exports.handler = async (event, context) => {
+    switch (event.httpMethod) {
+        case "GET":
+            if (!event.queryStringParameters.x) { return { statusCode: 400, body: "400 Bad Request", } }
+            let Xs = [
+                "0.0.1"
+            ];
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET"
+                },
+                body: JSON.stringify({
+                    x: (() => {
+                        try {
+                            return Xs.includes(atob(atob(event.queryStringParameters.x)))
+                        } catch { return false }
+                    })()
+                }),
+            };
+            break;
+        default:
+            return {
+                statusCode: 405,
+                body: "405 Method Not Allowed",
+            };
+            break;
+    }
+};
